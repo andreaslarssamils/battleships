78691337f19eef40c45c08d4ad1766d9b674fafb
andreaslarssamils <larssamils.andreas@gmail.com>
2025-07-23 19:33:37 +0200

    add the Board class and import random module

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random # Importing the random module
+
+class Board:
+    """
+        A class representing the game board for battleship with a constructor, print_board, place_ships, check_hit, all_ships_sunk functions.
+    """
+
